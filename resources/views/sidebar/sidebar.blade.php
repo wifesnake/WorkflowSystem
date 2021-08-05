67b96ebc828157c5d00fbafb9bf13d98e81aac14
wifesnake <wifesnake@gmail.com>
2021-08-06 01:10:10 +0700

    update sidebar, update gitignore, update getdata model

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -12,16 +12,6 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      {{-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset('storage/images/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div> --}}
-
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -29,11 +19,17 @@
                with font-awesome or any other icon font library -->
           @foreach ($menus as $menu)
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            @if ($menu->link !== NULL)
+              <a href="{{ url($menu->link) }}" class="nav-link">
+            @else
+              <a href="#" class="nav-link">
+            @endif
               <i class="nav-icon {{ $menu->icon }}"></i>
               <p>
                 {{ $menu->name }}
-                <i class="right fas fa-angle-left"></i>
+                @if ($menu->is_menu)
+                  <i class="right fas fa-angle-left"></i>  
+                @endif                
               </p>
             </a>
             @foreach ($submenus as $submenu)
@@ -48,20 +44,6 @@
                   </ul>
                 @endif
             @endforeach
-            {{-- <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
-            </ul> --}}
           </li>
           @endforeach
         </ul>
