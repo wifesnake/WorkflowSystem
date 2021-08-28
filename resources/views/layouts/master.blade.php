5ee5ea7e328c2bf152a6bbc62a268f86998767d4
wifesnake <wifesnake@gmail.com>
2021-08-28 22:35:50 +0700

    change login email to username

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,41 +6,43 @@ scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>AdminLTE 3 | Starter</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>AdminLTE 3 | Starter</title>
 
-  <!-- Styles -->
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
 
-  <style>
-    .table.dataTable{
-      width: 100% !important;
-    }
-  </style>
+    {{-- Script --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
+
+    <style>
+        .table.dataTable{
+        width: 100% !important;
+        }
+    </style>
 
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+    <div class="wrapper">
 
-  @include('navbar.navbar')
+    @include('navbar.navbar')
 
-  @include('sidebar.sidebar')
+    @include('sidebar.sidebar')
 
-  @yield('content')
+    @yield('content')
 
-  @include('footer.footer')
+    @include('footer.footer')
 
-</div>
-<!-- ./wrapper -->
+    </div>
+    <!-- ./wrapper -->
 
-<!-- REQUIRED SCRIPTS -->
-<script src="{{ mix('js/app.js') }}" defer></script>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
+    <!-- REQUIRED SCRIPTS -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
