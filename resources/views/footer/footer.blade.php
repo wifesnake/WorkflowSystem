21feef41b838c309c9f1db4112221eb427081304
DESKTOP-7IMNQB0\Yoskung <yos_007@hotmail.com>
2021-08-29 14:12:08 +0700

    no message

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -1,7 +1,6 @@
 <!-- Main Footer -->
 <footer class="main-footer">
     <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">Anything you want</div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2021 VICHIAN TRANSPORT LIMITED PARTNERSHIP.</strong> 
   </footer>
