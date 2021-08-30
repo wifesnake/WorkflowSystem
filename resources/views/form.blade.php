a0474dc0cf4d7c123eda615ce7193aa86de99ea4
wifesnake <wifesnake@gmail.com>
2021-08-31 03:49:16 +0700

    update dynamic button (back,submit,approve,cancel)  by current state

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -9,11 +9,11 @@
         <div class="content">
             <div class="container-fluid">
                 @foreach ($flowdatas as $flowdata)
-                    <input type="hidden" name="orderno" id="orderno" value="{{ $flowdata->ord_vehicle }}">
-                    <input type="hidden" name="prevstate" id="prevstate" value="{{ $flowdata->prev_state }}">
-                    <input type="hidden" name="currentstate" id="currentstate" value="{{ $flowdata->current_state }}">
-                    <input type="hidden" name="nextstate" id="nextstate" value="{{ $flowdata->next_state }}">
-                    <input type="hidden" name="updatedby" id="updatedby" value="{{ Auth::user()->name }}">
+                    <input type="text" name="orderno" id="orderno" value="{{ $flowdata->ord_vehicle }}">
+                    <input type="text" name="prevstate" id="prevstate" value="{{ $flowdata->prev_state }}">
+                    <input type="text" name="currentstate" id="currentstate" value="{{ $flowdata->current_state }}">
+                    <input type="text" name="nextstate" id="nextstate" value="{{ $flowdata->next_state }}">
+                    <input type="text" name="updatedby" id="updatedby" value="{{ Auth::user()->name }}">
 
                     <h1>State {{ __($flowdata->current_state) }} - {{ __($flowdata->state_name) }}</h1>
 
@@ -24,10 +24,18 @@
             </div>
             <!-- /.container-fluid -->
         </div>
+
+        @if ($is_admin == 1)
+            <div style="position: absolute; bottom: 0; padding: 10px 5px;">
+                <button type="submit" class="btn btn-primary">Request</button>
+            </div>
+        @else
+            <div style="position: absolute; bottom: 0; padding: 10px 5px;">
+                <button type="submit" class="btn btn-primary">Approve</button>
+            </div>
+        @endif
+
         <!-- /.content -->
-        {{-- <div style="position: absolute; bottom: 0; padding: 10px 5px;">
-            <button type="submit" class="btn btn-primary">Request</button>
-        </div> --}}
     </div>
     <!-- /.content-wrapper -->
 </form>
