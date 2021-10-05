bcdb69feb9d68e60717b745f56ec4791d31ff85d
DESKTOP-7IMNQB0\Yoskung <yos_007@hotmail.com>
2021-10-06 00:55:11 +0700

    no message

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -11,111 +11,121 @@
     <div class="content">
         <div class="container-fluid">
 
-            <form action="#" id="f-employee">
-                <div class="group_data">
-                    <div class="col-md-12">
-                        <div class="title-form">
-                            ข้อมูลพนักงาน
-                        </div>
-                    </div>
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            คำนำหน้า / ชื่อ - สกุล :
-                        </div>
-                        <div class="col-md-1">
-                            <select name="titlename" id="titlename" class="form-control" required>
-                                <option value="">-- Please Select --</option>
-                                @foreach ($titlenames as $item)
-                                <option value="{{ $item->code_lookup }}">{{ $item->value_lookup }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-4">
-                            <input type="text" name="name" id="name" class="form-control" required>
-                        </div>
-                        <div class="col-md-4">
-                            <input type="text" name="lastname" id="lastname" class="form-control" required>
-                        </div>
-                    </div>
+            <div class="btn-collapse">
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapse"
+                    aria-expanded="false" aria-controls="collapse">
+                    <i class="fa fa-plus-square" aria-hidden="true"></i>เพิ่มข้อมูลพนักงาน
+                </button>
+            </div>
+            <div class="collapse" id="collapse">
 
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            ที่อยู่ :
-                        </div>
-                        <div class="col-md-9">
-                            <textarea name="address" id="address" class="form-control" rows="4"></textarea>
-                        </div>
-                    </div>
+                <form action="#" id="f-employee">
+                    <div class="group_data">
 
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            เลขที่บัตรประชาชน :
-                        </div>
-                        <div class="col-md-9">
-                            <input type="text" name="id_card" id="id_card" class="form-control" required>
-                        </div>
-                    </div>
+                        <div class="menu-action col-md-12">
 
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            ประเภทพนักงาน / รหัสพนักงาน :
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
-                        <div class="col-md-2">
-                            <select name="employee_type" id="employee_type" class="form-control" required>
-                                <option value="">-- Please Select --</option>
-                            </select>
+
+                        <div class="col-md-12">
+                            <div class="title-form">
+                                ข้อมูลพนักงาน
+                            </div>
                         </div>
-                        <div class="col-md-7">
-                            <input type="text" name="employee_id" id="employee_id" class="form-control"
-                                required>
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                คำนำหน้า / ชื่อ - สกุล :
+                            </div>
+                            <div class="col-md-1">
+                                <select name="titlename" id="titlename" class="form-control" required>
+                                    <option value="">-- Please Select --</option>
+                                    @foreach ($titlenames as $item)
+                                    <option value="{{ $item->code_lookup }}">{{ $item->value_lookup }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" name="name" id="name" class="form-control" required>
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" name="lastname" id="lastname" class="form-control" required>
+                            </div>
                         </div>
+
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                ที่อยู่ :
+                            </div>
+                            <div class="col-md-9">
+                                <textarea name="address" id="address" class="form-control" rows="4"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                เลขที่บัตรประชาชน :
+                            </div>
+                            <div class="col-md-9">
+                                <input type="text" name="id_card" id="id_card" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                ประเภทพนักงาน / รหัสพนักงาน :
+                            </div>
+                            <div class="col-md-2">
+                                <select name="employee_type" id="employee_type" class="form-control" required>
+                                    <option value="">-- Please Select --</option>
+                                </select>
+                            </div>
+                            <div class="col-md-7">
+                                <input type="text" name="employee_code" id="employee_code" class="form-control"
+                                    required>
+                            </div>
+
+                        </div>
+
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                E-mail :
+                            </div>
+                            <div class="col-md-9">
+                                <input type="text" name="email" id="email" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                หมายเลขโทรศัพท์ :
+                            </div>
+                            <div class="col-md-9">
+                                <input type="text" name="phone" id="phone" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                เงินเดือน :
+                            </div>
+                            <div class="col-md-9">
+                                <input type="text" name="salary" id="salary" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="row col-md-12">
+                            <div class="col-md-3">
+                                Department :
+                            </div>
+                            <div class="col-md-9">
+                                <input type="text" name="department" id="department" class="form-control" required>
+                            </div>
+                        </div>
+
 
                     </div>
                 </form>
             </div>
-
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            E-mail :
-                        </div>
-                        <div class="col-md-9">
-                            <input type="text" name="email" id="email" class="form-control" required>
-                        </div>
-                    </div>
-
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            หมายเลขโทรศัพท์ :
-                        </div>
-                        <div class="col-md-9">
-                            <input type="text" name="phone" id="phone" class="form-control" required>
-                        </div>
-                    </div>
-
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            เงินเดือน :
-                        </div>
-                        <div class="col-md-9">
-                            <input type="text" name="salary" id="salary" class="form-control" required>
-                        </div>
-                    </div>
-
-                    <div class="row col-md-12">
-                        <div class="col-md-3">
-                            Department :
-                        </div>
-                        <div class="col-md-9">
-                            <input type="text" name="department" id="department" class="form-control" required>
-                        </div>
-                    </div>
-
-                    <div class="menu-action col-md-12">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-
-                </div>
-            </form>
             <div class="card mt-2">
                 <section class="m-2">
                     <div class="col-md-12 table-responsive">
@@ -132,92 +142,52 @@
 <!-- /.content-wrapper -->
 
 <script>
-    function onDelete(id,name){
-        var result = confirm("Are you want to delete "+ name +" ?");
-        if (result) {
-            var settings = {
-                "url": "{{url('api/employee')}}/"+id,
-                "method": "DELETE",
-                "timeout": 0,
-                "headers": {
-                "Content-Type": "application/x-www-form-urlencoded"
-                },
-                "data": {}
-            };
+$('#f-employee').submit(function($this) {
+    var settings = {
+        "url": "{{url('api/employee')}}",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "titlename": $("select[name~='titlename']").val() == "" ? "" : $("select[name~='titlename']")
+                .val(),
+            "name": $("input[name~='name']").val(),
+            "lastname": $("input[name~='lastname']").val(),
+            "address": $("input[name~='address']").val(),
+            "id_card": $("input[name~='id_card']").val(),
+            "employee_code": $("input[name~='employee_code']").val(),
+            "employee_type": $("input[name~='employee_type']").val(),
+            "email": $("input[name~='email']").val(),
+            "phone": $("input[name~='phone']").val(),
+            "salary": $("input[name~='salary']").val(),
+            "department": $("input[name~='department']").val(),
+            "created_by": '{{ Auth::user()->name }}',
+            "updated_by": '{{ Auth::user()->name }}'
 
-            $.ajax(settings).done(function (response,status) {
-                if(status=="success"){
-                    window.location.href = "{{url('/employee')}}"
-                }
-            });
+            // "titlename": "a",
+            // "name": "a",
+            // "lastname": "a",
+            // "address": "a",
+            // "id_card": "a",
+            // "employee_code": "a",
+            // "employee_type": "a",
+            // "email": "a",
+            // "phone": "a",
+            // "salary": "10",
+            // "department": "a",
+            // "created_by": "admin",
+            // "updated_by": "admin"
         }
-    }
+    };
 
-    // $('#f-employee').submit(function($this){
-    function test(){
-        var settings = {
-            "url": "{{url('api/employee')}}/" + id,
-            "method": "DELETE",
-            "timeout": 0,
-            "headers": {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            "data": {}
-        };
+    console.log(settings);
 
-        $.ajax(settings).done(function(response) {
-            if (status == "success") {
-                window.location.href = "{{url('/employee')}}"
-            }
-        });
-    }
-
-    $('#f-employee').submit(function($this) {
-        var settings = {
-            "url": "{{url('api/employee')}}",
-            "method": "POST",
-            "timeout": 0,
-            "headers": {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            "data": {
-                "titlename": $("select[name~='titlename']").val() == "" ? "" : $("select[name~='titlename']")
-                    .val(),
-                "name": $("input[name~='name']").val(),
-                "lastname": $("input[name~='lastname']").val(),
-                "address": $("input[name~='address']").val(),
-                "id_card": $("input[name~='id_card']").val(),
-                "employee_id": $("input[name~='employee_id']").val(),
-                "employee_type": $("input[name~='employee_type']").val(),
-                "email": $("input[name~='email']").val(),
-                "phone": $("input[name~='phone']").val(),
-                "salary": $("input[name~='salary']").val(),
-                "department": $("input[name~='department']").val(),
-                "created_by": '{{ Auth::user()->name }}',
-                "updated_by": '{{ Auth::user()->name }}'
-
-                // "titlename": "a",
-                // "name": "a",
-                // "lastname": "a",
-                // "address": "a",
-                // "id_card": "a",
-                // "employee_id": "a",
-                // "employee_type": "a",
-                // "email": "a",
-                // "phone": "a",
-                // "salary": "10",
-                // "department": "a",
-                // "created_by": "admin",
-                // "updated_by": "admin"
-            }
-        };
-
-        console.log(settings);
-
-        $.ajax(settings).done(function(response) {
-            console.log(response);
-        });
+    $.ajax(settings).done(function(response) {
+        console.log(response);
     });
+});
 </script>
 
 @endsection
