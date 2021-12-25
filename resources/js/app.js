1aaef2742005c99d658c3232300f21a19625bb0f
ADMIN682B\admin <wifesnake@gmail.com>
2021-12-26 04:33:02 +0700

    fix asset js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 require('./bootstrap');
-// require('../assets/js/jquery-3.5.1.min');
-require('../assets/js/jquery.dataTables.min');
-require('../assets/js/dataTables.bootstrap4.min')
+require('../assets/js/jquery-3.5.1.min');
+require('../assets/js/sweetalert.min');
+require('../assets/js/jquery-ui.min');
+require('../assets/js/jquery.signature');
+
