62841a839e9919c2f436bfeed55c15fb12d83ece
ADMINF825\admin <wifesnake@gmail.com>
2022-01-03 01:17:22 +0700

    update https

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,7 +22,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="{{ mix('js/app.js') }}" defer></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
   
     
