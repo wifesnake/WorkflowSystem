c0a197d2978b7ef15891951561cd38b810d9af50
ADMINF825\admin <wifesnake@gmail.com>
2022-12-07 03:05:12 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -61,9 +61,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="{{ asset('js/vfs_fonts.js') }}"></script>
     <script src="{{ asset('js/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('js/buttons.print.min.js') }}"></script>
-    {{-- <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="http://cdn.datatables.net/1.10.7/js/jquery.dataTables.js"></script> --}}
-    <script src="https://cdn.jsdelivr.net/gh/ashl1/datatables-rowsgroup@fbd569b8768155c7a9a62568e66a64115887d7d0/dataTables.rowsGroup.js"></script>
+    <script src="{{ asset('js/dataTables.rowsGroup.js') }}"></script>
 
 </head>
 <body class="hold-transition sidebar-mini">
